f31b8b39a4bb675aa61138dbe6f70bce31f6a31b
N0W3N <richynh93@gmail.com>
2018-07-14 01:03:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ into another file format or file extension.
 I made this tool because it can be very frustrating to manually convert a lot of files into another file format (e.g. ts -> mp4)
 and a hell of waste of time.
 I do recommend to start the script via cmd / terminal, because it doesn't show the progress inside your IDE (atleast PyCharm)
-It has been successfully tested under Windows 10 with Python 3.7 and the newest ffmpeg version.
+It has been successfully tested under Windows 10 with Python 3.7 and the newest ffmpeg version."""
 
 # import all necessarily modules
 
