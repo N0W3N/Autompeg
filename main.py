b8784f42817853d2ea6c97ad93b33bf05c3d0b76
N0W3N <15861569+N0W3N@users.noreply.github.com>
2019-10-05 03:08:06 +0200

    Fix minor grammar issues
    Signed-off-by: N0W3N <15861569+N0W3N@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import os.path
 import subprocess
 import sys
 
-# working dir and extenstion type will be passed through CLI
+# working dir and extension type will be passed through CLI
 
 workDir = sys.argv[1]
 extType = sys.argv[2]
 newExtType = sys.argv[3]
 
-# exception-clause to prevent a faulty workdir and ffmpeg process
+# exception-clause to prevent a faulty WorkDir and ffmpeg process
 try:
-    filesInWorkDir = os.listdir(workDir)  #cache all the existig files in the directory
+    filesInWorkDir = os.listdir(workDir)  # cache all the existing files in the directory
 except IOError:
     print("Filepath not found. Please check the location of your media file(s).\n")
 else:
     for file in filesInWorkDir:
-        if str(file).split('.')[-1] == extType:  #scan for files with the extension given in 'exttype'
-            newFile = str(file).split('.' + extType)[0] + '.mp4' #  replace the extension with 'newExtType'
+        if str(file).split('.')[-1] == extType:  # scan for files with the extension given in 'extType'
+            newFile = str(file).split('.' + extType)[0] + '.mp4'  # replace the extension with 'newExtType'
             filepath = workDir + '\\' + file
             newfilepath = workDir + '\\' + newFile
 
@@ -26,3 +26,5 @@ else:
 
             subprocess.call(['E:\\ffmpeg\\ffmpeg.exe', '-i', filepath, '-c:v', 'copy', '-bsf:a', 'aac_adtstoasc',
                              newfilepath])
+        else:
+            print("No media found. Rerun the script and check the directory.")
