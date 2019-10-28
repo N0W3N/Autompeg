998b8c493e7e5edc3ab2536153b4792a3640036e
Riccardo <richynh93gmail.com>
2019-10-28 22:38:19 +0100

    Refactor code Comment ffmpeg arguments

--- autompeg.py
+++ autompeg.py
@@ -18,12 +18,22 @@ except IOError:
     print("Filepath not found. Please check the location of your media file(s).\n")
 else:
     for file in filesInWorkDir:
-        if str(file).split('.')[-1] == extType:  # scan for files with the extension given in 'extType'
-            newFile = str(file).split('.' + extType)[0] + newExtType  # replace the extension with 'newExtType'
-            filepath = workDir + '\\' + file
-            newfilepath = workDir + '\\' + newFile
+        if str(file).split(".")[-1] == extType:  # scan for files with the extension given in 'extType'
+            newFile = str(file).split("." + extType)[0] + newExtType  # replace the extension with 'newExtType'
+            filepath = workDir + "\\" + file
+            newfilepath = workDir + "\\" + newFile
 
             # no need to include an exception-clause here yet, since ffmpeg automatically detects a faulty filepath
 
-            subprocess.call(['E:\\ffmpeg\\ffmpeg.exe', '-i', filepath, '-c:v', 'copy', '-bsf:a', 'aac_adtstoasc',
-                             newfilepath])
+            subprocess.call(
+                [
+                    "E:\\ffmpeg\\ffmpeg.exe", # path of ffmpeg
+                    "-i", # input argument for file
+                    filepath, # file path of the 'old' media file
+                    "-c:v", # select video stream
+                    "copy", # copy video stream and don't convert it (to prevent quality loss)
+                    "-bsf:a", # select bitstream filter for the audio stream
+                    "aac_adtstoasc", # remove the ADTS header from the audio stream
+                    newfilepath, # file path of the 'new' media file
+                ]
+            )
