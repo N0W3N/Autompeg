36fcd804dfc98e90552f6c66bd209a7906874319
riccardo <15861569+N0W3N@users.noreply.github.com>
2020-02-17 00:39:42 +0100

    Reformat Code -> usage of f-strings -> check and if clause in case config file or path already exists

--- install.py
+++ install.py
@@ -1,15 +1,36 @@
 from configparser import ConfigParser
-
+import sys
+import os
 
 config = ConfigParser(allow_no_value=True)
 
-ffmpeg_path = input("Enter the path where ffmpeg is located: ")
+def checker():
+    ffmpeg_path = input("File path of FFmpeg: ")
+    if os.path.exists('config.ini'):
+        print('Config file already exists.')
+        with open('config.ini', 'r') as cfg:
+            path = config.get('Path of ffmpeg', 'path')
+            if path in ffmpeg_path:
+                print("File path is also the same as in the config file.")
+                pass
+                sys.exit()
+    else:
+        return ffmpeg_path
 
-config.add_section('Path of ffmpeg')
-config.set('Path of ffmpeg', 'Path', str(ffmpeg_path))
 
-try:
+def config_writer(ffmpeg_path):
+    config.add_section('Path of ffmpeg')
+    config.set('Path of ffmpeg', 'Path', str(ffmpeg_path))
+
     with open('config.ini', 'w') as cfg:
         config.write(cfg)
-except IOError:
-    print("File either doesn't exist or isn't writeable. \n")
+        print('Config has successfully created. \n'
+              f'Path of ffmpeg as been set to {ffmpeg_path}')
+
+
+def main():
+    config_writer(checker())
+
+
+if __name__ == '__main__':
+    main()
