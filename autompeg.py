fa60c2803f781d62068b7c83d5c66c787d4d95f4
riccardo <15861569+N0W3N@users.noreply.github.com>
2020-03-17 03:10:11 +0100

    Fix whitespace issue with filepaths under Mac environment -> subprocess now takes f'{}' - arguments instead of simple variables as user input.  -> Filepaths will be now passed as double quotes (real strings) and eliminate the whole white space issue completely
    Replace os.listdir with os.walk
    -> Make recursive search available
    -> Way faster creating and processing data structures / trees
     -> Autompeg now looks over all folder and files for the given file extension
     -> Could be possible to make this recursive feature an optional feature in the future
    
    Currently pushed through Mac branch, will be merged with master branch, after testing the script under Windows environment.

--- autompeg.py
+++ autompeg.py
@@ -28,33 +28,41 @@ except IOError:
 
 # exception-clause to prevent a faulty WorkDir and therefore the following ffmpeg process
 
-try:
-    filesInWorkDir = os.listdir(workDir)  # cache all the existing files in the directory
+"""try:
+    filesInWorkDir = os.walk(workDir)  # cache all the existing files in the directory
 except IOError:
     print("Filepath not found. Please check the location of your media file(s).\n")
-else:
-    for file in filesInWorkDir:
+else:"""
+for root, directories, filenames in os.walk(workDir):
+    for filename in filenames:
+        filename = os.path.join(root, filename)
+
         if sys.platform.startswith('win32'):
-            newfilename = str(file.split(".") + extType[0] + newExtType)
+            newfilename = str(filename.split(".") + extType[0] + newExtType)
             slash = "\\"
         elif sys.platform.startswith('darwin'):
-            newfilename = str(file.split(extType)[0] + newExtType)
+            newfilename = str(filename.split(extType)[0] + newExtType)
             slash = "//"
-            if file.endswith(extType):  # scan for files with the extension given in 'extType'
-                filepath = workDir + slash + file
-                newfilepath = workDir + slash + newfilename
+            if filename.endswith(extType):  # scan for files with the extension given in 'extType'
+                filepath = filename
+                #filepath = workDir + slash + filename
+                #newfilepath = workDir + slash + newfilename
+                newfilepath = newfilename
+
+                print(filepath)
+                print(newfilepath)
 
                 # no need to include an exception-clause here yet, since ffmpeg automatically detects a faulty filepath
 
                 subprocess.run(
-                        [
-                            path,  # path of ffmpeg
-                            "-i",  # input argument for file
-                            filepath,  # file path of the 'old' media file
-                            "-c:v",  # select video stream
-                            "copy",  # copy video stream and don't convert it (to prevent quality loss)
-                            "-bsf:a",  # select bitstream filter for the audio stream
-                            "aac_adtstoasc",  # remove the ADTS header from the audio stream
-                            newfilepath,  # file path of the 'new' media file
-                        ]
-                    )
+                    [
+                        path,  # path of ffmpeg
+                        "-i",  # input argument for file
+                        f'{filepath}',  # file path of the old media file
+                        "-c:v",  # select video stream
+                        "copy",  # copy video stream and don't convert it (to prevent quality loss)
+                        "-bsf:a",  # select bitstream filter for the audio stream
+                        "aac_adtstoasc",  # remove the ADTS header from the audio stream
+                        f'{newfilepath}',  # file path of the 'new' media file
+                    ]
+                )
