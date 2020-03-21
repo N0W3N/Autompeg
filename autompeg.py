5f8a1b8c6af8a846628eccf876335f044f42b695
riccardo <15861569+N0W3N@users.noreply.github.com>
2020-03-21 13:24:38 +0100

    Rework filepath / name manipulation
    Code now uses os.path.splitext to create the new filename based on it old filename.
    This also removes the redundant code block where it check the running OS and creates filepaths/names based on that information.
    Part of this code block has been moved up to check the given filepath via CLI and replace any slashes with backward-slashes in case the running OS is Windows.
    
    - Code cleanup
    -> Removed comments and dummy code blocks which are unnecessary

--- autompeg.py
+++ autompeg.py
@@ -11,9 +11,11 @@ try:
     workDir = sys.argv[1]
     extType = sys.argv[2]
     newExtType = sys.argv[3]
+
 except IndexError:
     raise Exception("Usage: python3 autompeg.py <path to workfolder> <old fileformat> <new fileformat>"
-                    "e.g. python3 autompeg.py /Volumes/Volume1/Work .ts .mp4")
+                    "e.g. (Windows) python3 autompeg.py C:\\Users\\Test\\Work .ts .mp4"
+                    "e.g. (Mac) python3 autompeg.py /Volumes/Volume1/Work .ts .mp4")
 
 # Config Parser
 
@@ -29,43 +31,30 @@ except IOError:
 
 # exception-clause to prevent a faulty WorkDir and therefore the following ffmpeg process
 
-"""try:
-    filesInWorkDir = os.walk(workDir)  # cache all the existing files in the directory
-except IOError:
-    print("Filepath not found. Please check the location of your media file(s).\n")
-else:"""
+if sys.platform.startswith('win32'):
+    workDir = workDir.replace('/', '\\')
+else:
+    pass
+
 for root, directories, filenames in os.walk(workDir):
     for filename in filenames:
         filename = os.path.join(root, filename)
+        newfilename = os.path.splitext(filename)[0] + newExtType
+        if filename.endswith(extType):  # scan for files with the extension given in 'extType'
+            filepath = filename
+            newfilepath = newfilename
 
-        if sys.platform.startswith('win32'):
-            newfilename = os.path.splitext(filename)[0] + newExtType
-            #newfilename = str(filename.split(".") + extType[0] + newExtType)
-            slash = "\\"
-        elif sys.platform.startswith('darwin'):
-            newfilename = os.path.splitext(filename)[0] + newExtType
-            #newfilename = str(filename.split(extType)[0] + newExtType)
-            slash = "//"
-            if filename.endswith(extType):  # scan for files with the extension given in 'extType'
-                filepath = filename
-                #filepath = workDir + slash + filename
-                #newfilepath = workDir + slash + newfilename
-                newfilepath = newfilename
+            # no need to include an exception-clause here yet, since ffmpeg automatically detects a faulty filepath
 
-                print(filepath)
-                print(newfilepath)
-
-                # no need to include an exception-clause here yet, since ffmpeg automatically detects a faulty filepath
-
-                subprocess.run(
-                    [
-                        path,  # path of ffmpeg
-                        "-i",  # input argument for file
-                        f'{filepath}',  # file path of the old media file
-                        "-c:v",  # select video stream
-                        "copy",  # copy video stream and don't convert it (to prevent quality loss)
-                        "-bsf:a",  # select bitstream filter for the audio stream
-                        "aac_adtstoasc",  # remove the ADTS header from the audio stream
-                        f'{newfilepath}',  # file path of the 'new' media file
-                    ]
-                )
+            subprocess.run(
+                [
+                    path,  # path of ffmpeg
+                    "-i",  # input argument for file
+                    f'{filepath}',  # file path of the old media file
+                    "-c:v",  # select video stream
+                    "copy",  # copy video stream and don't convert it (to prevent quality loss)
+                    "-bsf:a",  # select bitstream filter for the audio stream
+                    "aac_adtstoasc",  # remove the ADTS header from the audio stream
+                    f'{newfilepath}',  # file path of the 'new' media file
+                ]
+            )
